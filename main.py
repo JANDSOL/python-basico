be4c77844d6ed607b61c1907c9bb6078fca940b1
Juan Pablo <jpas.juan01@gmail.com>
2025-04-30 21:57:27 -0500

    feat: Explicación python básico
    Variables, constantes, tipos de datos y operadores.

---
+++ main.py
@@ -0,0 +1,126 @@
+# Start: Sintaxis
+# if True:
+#     print("Hola mundo")
+# End: Sintaxis
+
+# Start: Variables, constantes y tipos de datos
+# Variables
+# x = 10  # Guarda el número entero 10 en la variable X.
+# tasa_interes = 3.14
+# usuario_actual = "Ana"
+# usuario_activo = True
+
+# Constantes
+# MAX_INTENTOS_JUGADOR = 3 # ESTE VALOR NO DEBE CAMBIAR NUNCA
+
+# Print (stdout)
+# nombre = "Ana"
+# otro_nombre = "Pedro"
+# print("¡Hola, " + nombre + "!") # Imprime: ¡Hola, Ana!
+# print("¡Hola,", nombre + "!") # Imprime: ¡Hola, Ana!
+# print(f"¡Hola, {nombre} y {otro_nombre}!") # Imprime: ¡Hola, Ana y Pedro!
+
+# Tipos de datos básicos
+# int: números enteros (-2, 0, 5)
+# float: números decimales (3.14, -0.5)
+# str: texto entre comillas ("hola", 'hola')
+# bool: verdadero o falso (True, False)
+# print(type(x))
+# print(type(tasa_interes))
+# print(type(usuario_actual))
+# print(type(usuario_activo))
+
+# Tipado dinámico
+# numero = 10  # Python infiere que 'numero' es un entero.
+# print(type(numero))
+# nombre = "Juan"  # Aquí infiere que 'nombre' es una cadena.
+# print(type(nombre))
+
+# Declaración y asignación de variables
+# mensaje = ""
+# End: Variables, constantes y tipos de datos
+
+# Start: Operadores aritméticos
+# Operadores aritméticos
+# a = 10
+# b = 3
+# print(a + b)  # Suma → 13
+# print(a - b)  # Resta → 7
+# print(a * b)  # Multiplicación → 30
+# print(a / b)  # División → 3.333...
+# print(a // b) # División entera → 3
+# print(a % b)  # Módulo (resto de la división) → 1
+# print(a ** b) # Potencia → 1000 (10^3)
+
+# Operadores de comparación
+# x = 5
+# y = 10
+# print(x == y)  # ¿Son iguales? → False
+# print(x != y)  # ¿Son diferentes? → True
+# print(x > y)   # ¿x es mayor que y? → False
+# print(x < y)   # ¿x es menor que y? → True
+# print(x >= y)  # ¿x es mayor o igual que y? → False
+# print(x <= y)  # ¿x es menor o igual que y? → True
+
+# Operadores lógicos (and, or, not)
+# edad = 20
+# es_estudiante = True
+# print((edad > 18) and es_estudiante)  # True (ambas condiciones son ciertas)
+# print((edad > 18) and not es_estudiante)  # True (ambas condiciones son ciertas)
+# print((edad < 18) or es_estudiante)  # True (al menos una es cierta)
+# print((edad < 18) or not es_estudiante)  # True (al menos una es cierta)
+# print(not es_estudiante)  # False (invierte el valor)
+
+# Operadores de asignación
+# x = 10  # Asignación simple
+# print(x)
+# x += 5  # Suma y asigna (x = x + 5)
+# print(x)
+# x -= 3  # Resta y asigna (x = x - 3)
+# print(x)
+# x *= 2  # Multiplica y asigna (x = x * 2)
+# print(x)
+# x /= 4  # Divide y asigna (x = x / 4)
+# print(x)
+# Otros ejemplos:
+# a, b, c = 5, 10, 15  # Asignación múltiple (asigna 5 a 'a', 10 a 'b' y 15 a 'c').
+# print(a, b, c)
+# p = q = r = 0  # Asignación del mismo valor a múltiples variables (asigna 0 a 'p', 'q' y 'r').
+# print(p, q, r)
+## Start: Intercambio directo
+# x, y = 10, 20
+# print(x, y)
+# x, y = y, x
+# print(x, y)
+## End: Intercambio directo
+
+# Start: Input (stdin)
+# nombre = input("¿Cuál es tu nombre? ")
+# print("Hola,", nombre + "!")
+
+# dato_1 = float(input("Dame el dato 1: "))
+# dato_2 = float(input("Dame el dato 2: "))
+# print(f"La suma de los dos números es: {dato_1 + dato_2}")
+# End: Input
+
+# Start: Conversión de tipos de datos básicos
+## Conversión implícita
+# a = 5 # int
+# b = 2.0 # float
+# c = a + b # float
+# print(type(a), type(b), type(c))
+## Conversión explícita
+# Conversión a entero
+# num_str = "20"
+# print(type(num_str))
+# num_int = int(num_str)  # num_int será 20
+# print(type(num_int))
+
+# Conversión a flotante
+# num = 10
+# print(type(num))
+# num_float = float(num)  # num_float será 10.0
+# print(type(num_float))
+# End: Conversión de tipos de datos básicos
+
+# End: Operadores aritméticos
