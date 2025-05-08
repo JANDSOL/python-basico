e5b04f103bd823fdce694cb0dd26d548c0ee521a
Juan Pablo <jpas.juan01@gmail.com>
2025-05-07 20:30:17 -0500

    feat: Métodos de los strings

---
+++ main.py
@@ -0,0 +1,36 @@
+# Start: Métodos de los strings
+## Mayúsculas
+# word = "Programa en Python 3"
+# word_upper = word.upper()
+# print(word + '\n' + word_upper)
+
+## Notación título
+# word = "programa en python 3"
+# word_title = word.title()
+# print(word + '\n' + word_title)
+
+## Reemplazo
+# word = "Programa En Python 3"
+# word_new = word.replace(" ", "_")
+# print(word + '\n' + word_new)
+
+## Eliminación de espacios
+# word = "    Programa En Python 3    "
+# word_lstrip = word.lstrip()
+# print('"' + word_lstrip + '"')
+# word = "    Programa En Python 3    "
+# word_rstrip = word.rstrip()
+# print('"' + word_rstrip + '"')
+# word = "    Programa En Python 3    "
+# word_strip = word.strip()
+# print('"' + word_strip + '"')
+
+## Separador
+# word = "Programa En Python 3"
+# word_split = word.split()
+# print(word_split)
+# email = "example@gmail.com"
+# email_split = email.split("@")
+# print(email_split)
+
+# End: Métodos de los strings
